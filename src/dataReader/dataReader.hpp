1e1010103ac1d194a9e71ff5bc4b055cf7549c96
aramperezdios <aramperezdios@gmail.com>
2023-11-19 22:07:12 +0000

    created class for reading data from multiple sources

---
+++ src/dataReader/dataReader.hpp
@@ -0,0 +1,15 @@
+#ifndef DATA_READER_H
+#define DATA_READER_H
+
+#include <string>
+#include <fstream>
+
+class dataReader {
+    std::string data;
+};
+
+class dataReaderFile: public dataReader {
+
+};
+
+#endif
